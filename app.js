6f7cbf19ceac59648adb1e94bd1f2e4fa46dcdee
TaeHyeon <sthyeon11@gmail.com>
2022-01-28 06:33:54 +0900

    Commit

--- app.js
+++ app.js
@@ -101,8 +101,9 @@ app.get("/googleformentire/:id", async (req,res) => {
 
 app.get("/googleformentire", async (req,res) => {
   const googleForm = await GoogleForm.findAll({
-    include: [{model : FormQuestion, include: FormQuestionOption, order: [["id", "DESC"]] }],
-    order: [["id", "DESC"]]
+    include: 
+    [{model : FormQuestion, include: [{model :FormQuestionOption, order: [["id", "ASC"]]} ], order: [["id", "ASC"]] }],
+    order: [["id", "ASC"]]
   })
   
   res.json({result: googleForm})
